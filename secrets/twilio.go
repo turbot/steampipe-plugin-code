256c8c03adbd25b6fef6b422c66c9ca9bcf68591
LalitTurbot <lalit@turbot.com>
2021-08-20 19:33:16 +0530

    Add secret regex matcher for twilio_access_key

---
+++ secrets/twilio.go
@@ -0,0 +1,33 @@
+package secrets
+
+import "regexp"
+
+// import (
+// 	"encoding/base64"
+// 	"fmt"
+// 	"net/http"
+// 	"strings"
+
+// 	"github.com/turbot/go-kit/types"
+// )
+
+func init() {
+	RegisterMatcher(&twilioAccessKey{})
+}
+
+type twilioAccessKey struct{}
+
+func (*twilioAccessKey) Type() string {
+	return "twilio_access_key"
+}
+
+func (*twilioAccessKey) DenyList() []*regexp.Regexp {
+	return []*regexp.Regexp{
+		regexp.MustCompile(`AC[a-z0-9]{32}`), // Account SID (ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx)
+		regexp.MustCompile(`SK[a-z0-9]{32}`), // Auth token (SKxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx)
+	}
+}
+
+func (*twilioAccessKey) Verify(secret string) (*bool, error) {
+	return nil, nil
+}
