44ccc379d76c50d51337635527df3ec95be0766c
LalitTurbot <lalit@turbot.com>
2021-08-20 19:15:15 +0530

    Add secret regex matcher for stripe_access_key

---
+++ secrets/stripe.go
@@ -0,0 +1,54 @@
+package secrets
+
+import "regexp"
+
+// import (
+// 	"encoding/base64"
+// 	"fmt"
+// 	"net/http"
+// 	"strings"
+
+// 	"github.com/turbot/go-kit/types"
+// )
+
+func init() {
+	RegisterMatcher(&stripeAccessKey{})
+}
+
+type stripeAccessKey struct{}
+
+func (*stripeAccessKey) Type() string {
+	return "stripe_access_key"
+}
+
+func (*stripeAccessKey) DenyList() []*regexp.Regexp {
+	return []*regexp.Regexp{
+		regexp.MustCompile(`(?:r|s)k_live_[0-9a-zA-Z]{24}`),
+	}
+}
+
+func (*stripeAccessKey) Verify(secret string) (*bool, error) {
+	return nil, nil
+}
+
+// func (*stripeAccessKey) Verify(secret string) (*bool, error) {
+// 	verify_url := "https://slack.com/api/auth.test"
+
+// 	client := &http.Client{}
+// 	req, _ := http.NewRequest("GET", verify_url, nil)
+// 	req.Header.Set("Authorization", fmt.Sprintf("Basic %s", base64.StdEncoding.EncodeToString([]byte(secret))))
+
+// 	resp, err := client.Do(req)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer resp.Body.Close()
+// 	if resp.StatusCode == http.StatusOK {
+// 		return types.Bool(true), nil
+// 	}
+// 	if strings.HasPrefix(secret, "rk_live") {
+// 		return nil, nil
+// 	}
+
+// 	return types.Bool(false), nil
+// }
