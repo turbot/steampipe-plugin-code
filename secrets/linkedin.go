c7e22b36bd2ce4855c9505fe8cf4c5dcdf3f4e71
LalitTurbot <lalit@turbot.com>
2021-08-23 12:14:09 +0530

    update secret regex matcher for linkedin_secret_key

---
+++ secrets/linkedin.go
@@ -0,0 +1,25 @@
+package secrets
+
+import "regexp"
+
+func init() {
+	RegisterMatcher(&linkedinSecretKey{})
+}
+
+type linkedinSecretKey struct{}
+
+func (*linkedinSecretKey) Type() string {
+	return "linkedin_secret_key"
+}
+
+func (*linkedinSecretKey) DenyList() []*regexp.Regexp {
+	return []*regexp.Regexp{
+		regexp.MustCompile(`[0-9a-zA-Z]{16}`),
+	}
+}
+
+func (*linkedinSecretKey) Verify(secret string) (*bool, error) {
+	return nil, nil
+}
+
+// select * from code_secret where src = 'Xzx72LhHRdELIaxF';
