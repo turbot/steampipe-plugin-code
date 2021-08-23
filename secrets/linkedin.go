d676a229e41a773a41c0e3737c7a87623533829e
LalitTurbot <lalit@turbot.com>
2021-08-23 19:30:16 +0530

    update

--- secrets/linkedin.go
+++ secrets/linkedin.go
@@ -14,7 +14,7 @@ func (*linkedinSecretKey) Type() string {
 
 func (*linkedinSecretKey) DenyList() []*regexp.Regexp {
 	return []*regexp.Regexp{
-		regexp.MustCompile(`(?im)(?R)[0-9a-zA-Z]{16}`),
+		regexp.MustCompile(`(?im)[0-9a-zA-Z]{16}`),
 	}
 }
 
