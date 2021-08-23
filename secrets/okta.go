57fe4e2cc1916d548945a184fc7c7bd6aa1c582a
LalitTurbot <lalit@turbot.com>
2021-08-23 19:11:02 +0530

    Add regex matcher for okta_token

---
+++ secrets/okta.go
@@ -0,0 +1,25 @@
+package secrets
+
+import "regexp"
+
+func init() {
+	RegisterMatcher(&oktaToken{})
+}
+
+type oktaToken struct{}
+
+func (*oktaToken) Type() string {
+	return "okta_token"
+}
+
+func (*oktaToken) DenyList() []*regexp.Regexp {
+	return []*regexp.Regexp{
+		regexp.MustCompile(`00[a-zA-Z0-9\-\_]{40}`), // https://devforum.okta.com/t/api-token-length/5519
+	}
+}
+
+func (*oktaToken) Verify(secret string) (*bool, error) {
+	return nil, nil
+}
+
+// select * from code_secret where src = '02d0YZgNSJwlNew6lZG-6qGThisisatest-token'
