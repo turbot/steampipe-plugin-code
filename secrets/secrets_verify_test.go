66e60ff8c88bde71b4746c3547a9eeb932e1aeff
LalitTurbot <lalit@turbot.com>
2021-08-24 17:56:22 +0530

    update

--- secrets/secrets_verify_test.go
+++ secrets/secrets_verify_test.go
@@ -19,14 +19,14 @@ func TestSlackTokenVerify(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			fmt.Printf("TestSlackTokenVerify verification result: %s", *result)
+			fmt.Printf("TestSlackTokenVerify verification result: [%s]", *result)
 		}
 
 	}
 }
 
-// go test -v -run TestStripVerifyOk /Users/lalitbhardwaj/Turbot/prod/steampipe-plugin-code/secrets
-func TestStripVerifyOk(t *testing.T) {
+// go test -v -run TestStripeVerifyOk /Users/lalitbhardwaj/Turbot/prod/steampipe-plugin-code/secrets
+func TestStripeVerifyOk(t *testing.T) {
 	fmt.Println()
 	stripeApiKey := "sk_test_tR3PYbcVNZZ796tH88S4VQ2u"
 
@@ -36,16 +36,16 @@ func TestStripVerifyOk(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			fmt.Printf("TestStripVerifyOk verification result: %s", *result)
+			fmt.Printf("TestStripeVerifyOk verification result: [%s]", *result)
 		}
 
 	}
 }
 
-// go test -v -run TestStripVerifyFail /Users/lalitbhardwaj/Turbot/prod/steampipe-plugin-code/secrets
-func TestStripVerifyFail(t *testing.T) {
+// go test -v -run TestStripeVerifyFail /Users/lalitbhardwaj/Turbot/prod/steampipe-plugin-code/secrets
+func TestStripeVerifyFail(t *testing.T) {
 	fmt.Println()
-	stripeApiKey := "sk_test_tR3PYbcVNZZ796tH88S4VQ2b"
+	stripeApiKey := "sk_live_tR3PYbcVNZZ796tH88S4VQ2u"
 
 	for _, sm := range Matchers() {
 		if sm.Type() == "stripe_api_key" {
@@ -53,7 +53,11 @@ func TestStripVerifyFail(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			fmt.Printf("TestStripVerifyFail verification result: %s", *result)
+			fmt.Printf("TestStripeVerifyFail verification result: [%s]", *result)
 		}
 	}
 }
+
+// fmt.Println("String Output: \n", string(body))
+// fmt.Println("resp.Status: \n", resp.Status)
+// fmt.Println("resp.StatusCode: \n", resp.StatusCode)
