ab3702197e7f03c2d560e7c2a16d4ca2bf7e2c2f
cbruno10 <cody@turbot.com>
2021-09-27 16:10:26 -0400

    Fix reference link in aws_iam_access_key

--- secrets/aws_iam_access_key.go
+++ secrets/aws_iam_access_key.go
@@ -14,7 +14,7 @@ func (*awsAccessKeyID) Type() string {
 	return "aws_access_key_id"
 }
 
-// https://aws.amazon.com/blogs/security/a-safer-way-to-distribute-aws-credentials-to-ec2/''
+// https://aws.amazon.com/blogs/security/a-safer-way-to-distribute-aws-credentials-to-ec2/
 
 // grep -RP '(?<![A-Z0-9])[A-Z0-9]{20}(?![A-Z0-9])' *
 // grep -RP '(?<![A-Za-z0-9/+=])[A-Za-z0-9/+=]{40}(?![A-Za-z0-9/+=])' *
