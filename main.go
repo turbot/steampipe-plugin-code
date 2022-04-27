ff5e43ea136ee5b210a0bdd784afe056c2247da7
Ved misra <47312748+misraved@users.noreply.github.com>
2022-04-27 15:35:21 +0530

    Recompile plugin with steampipe-plugin-sdk-v3.1.0 and go v1.18 (#14)

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/turbot/steampipe-plugin-code/code"
-	"github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
 )
 
 func main() {
